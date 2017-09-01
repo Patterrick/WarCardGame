b541a5c6f028312722801b641beb25637a3c49d1
Patrick Purviance <purviance.pd@outlook.com>
2017-09-01 13:31:11 -0700

    added first option

--- realGame.py
+++ realGame.py
@@ -2,6 +2,9 @@ class my_class(object):
 	pass
 
 	print ("Hello, and welcome to text_based_game!");
+    Print ("You are in a dark room, you see a candle. What do you do?");
+    Print ("A) Pick up the candle");
+    Print ("B) Blow out the candle");
+    Print ("C) Eat the candle");
 
-
-
+    If (raw.input)
