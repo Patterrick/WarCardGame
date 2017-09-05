938925a071426a3ed4d9365864c6525c3d3e8183
theartur2000 <31487484+theartur2000@users.noreply.github.com>
2017-09-05 12:35:34 -0700

    Updated game to work
    Updated the game

--- realGame.py
+++ realGame.py
@@ -1,9 +1,8 @@
 class my_class(object):
     pass
 
-
-
     print ("Hello, and welcome to text_based_game!");
+    
 def question_1():
     print ("You are in a dark room, you see a candle. What do you do?");
     print ("A) Pick up the candle");
@@ -11,18 +10,18 @@ def question_1():
     print ("C) Eat the candle");
 
     answer = input("");
-    if answer == "A" or "a":
+    if answer == "A" or answer == "a":
         print ("You pick up the candle.")
         print ("")
         print ("")
 
-    elif answer == "B" or "b":
+    elif answer == "B" or answer == "b":
         print ("You blow out the candle and die from insanity")
         print ("")
         print ("")
         question_1()
 
-    elif answer == "C" or "c":
+    elif answer == "C" or answer == "c":
         print ("You are poisoned by the candle and die.")
         print ("")
         print ("")
