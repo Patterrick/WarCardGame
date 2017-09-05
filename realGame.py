243b21d702b26abde388862331aa1ebc61bc8ef2
theartur2000 <31487484+theartur2000@users.noreply.github.com>
2017-09-05 12:47:57 -0700

    Updated game
    added another question

--- realGame.py
+++ realGame.py
@@ -11,9 +11,10 @@ def question_1():
 
     answer = input("");
     if answer == "A" or answer == "a":
-        print ("You pick up the candle.")
+        print ("	You pick up the candle and walk forward. You hit a wall. It is the wall part of a hall. Which way do you go?")
         print ("")
         print ("")
+        question_2()
 
     elif answer == "B" or answer == "b":
         print ("You blow out the candle and die from insanity")
@@ -26,5 +27,22 @@ def question_1():
         print ("")
         print ("")
         question_1()
+
+def question_2():
+    print ("A) Right.");
+    print ("B) Left.");
+
+    answer = input("");
+    if answer == "A" or answer == "a":
+        print ("	You walk to the right and are greeted with a door with a rusty lock. What do you do?")
+        print ("")
+        print ("")
+
+    elif answer == "B" or answer == "b":
+        print ("You walk to the left and fall into a hole full of rusty spikes and die instantly.")
+        print ("")
+        print ("")
+        question_1()
+
 
 question_1()
